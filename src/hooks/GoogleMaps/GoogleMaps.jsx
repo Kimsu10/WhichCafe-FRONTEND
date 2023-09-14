adf33ba911f76b70f2eaa3ea260997a5d4d1b9d0
Kimsu <betnwjd25@gmail.com>
2023-09-14 10:19:42 +0900

    ADD: 현재위치로드

--- src/hooks/GoogleMaps/GoogleMaps.jsx
+++ src/hooks/GoogleMaps/GoogleMaps.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const MapContainer = () => {
@@ -6,18 +6,36 @@ const MapContainer = () => {
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
-  const center = { lat: 38, lng: 127 };
+  const [center, setCenter] = useState({ lat: 0, lng: 0 });
 
   const mapStyles = {
     width: '100%',
     height: '400px',
   };
 
-  if (loadError) return <div>위치 권한 동의 안할시 이미지 띄우기</div>;
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        error => {
+          console.error('Error getting user location:', error);
+        },
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
+  if (loadError) return <div>위치 권한 동의 안할 시 이미지 띄우기</div>;
 
   return (
     isLoaded && (
-      <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={10}>
+      <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={15}>
         <Marker position={center} />
       </GoogleMap>
     )
